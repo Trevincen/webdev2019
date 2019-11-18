cfa7b7203f584324e8f4c3dac78ff8e90bcba811
Trevincen <57086204+Trevincen@users.noreply.github.com>
2019-11-18 13:31:03 +0700

    backpage
    backpage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ url('/post/create') }}">Create</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}">Back to Page</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
